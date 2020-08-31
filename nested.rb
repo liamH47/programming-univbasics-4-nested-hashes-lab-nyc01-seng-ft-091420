a871ca3530fdd7bc0947b5e910cae021e7b36450
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-31 15:57:07 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def adding_matz
       :alan_kay => {
         :known_for => "Object Orientation",
         :languages => ["Smalltalk", "LISP"]
-      },
+      }
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]}
