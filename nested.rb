c8e11b2af4a8d06d689d7cefcac56b45cf80e4ea
friendly-machine-4891 <liamhanafee@gmail.com>
2020-08-31 15:25:23 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -26,20 +26,13 @@ end
 def dennis_ritchies_language
 	programmer_hash = 
  		{
-      :grace_hopper => {
-        :known_for => "COBOL",
-        :languages => ["COBOL", "FORTRAN"]
-      },
-      :alan_kay => {
-        :known_for => "Object Orientation",
-        :languages => ["Smalltalk", "LISP"]
-      },
+      
       :dennis_ritchie => {
         :known_for => "Unix",
         :languages => ["C"]
       }
     }
-
+  programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
